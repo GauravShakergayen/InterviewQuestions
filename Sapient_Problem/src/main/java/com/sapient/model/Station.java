4a166387611698d263671b3e49b7a12c2a9f36c3
Gaurav Shakergayen <g.shakergayen@gmail.com>
2016-08-09 22:05:22 +0530

    Latest Changes

--- Sapient_Problem/src/main/java/com/sapient/model/Station.java
+++ Sapient_Problem/src/main/java/com/sapient/model/Station.java
@@ -5,7 +5,7 @@ public enum Station {
 	A1, A2, A3, A4, A5, A6, A7, A8, A9, A10;
 
 	public int distance(Station other) {
-		System.out.println(other.ordinal()+"***********"+this.ordinal());
+
 		return Math.abs(other.ordinal() - this.ordinal());
 	}
 }
